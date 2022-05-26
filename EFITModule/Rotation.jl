de229b6f61e83c6cdb7e17ffe439e34112f348c7
Seth G <legoscout5241@outlook.com>
2022-05-26 08:32:55 -0400

    rotation debugging

--- EFITModule/Rotation.jl
+++ EFITModule/Rotation.jl
@@ -122,33 +122,57 @@ function rotMatrix(c,x,y,z;degrees=true)
 
 end
 #https://www.researchgate.net/profile/Abdalrhaman-Koko/post/How_to_rotate_an_anisotropic_stiffness_matrix_to_an_orthotropic_matrix_of_cancellous_bone/attachment/60b8c0eb5e24cd000161f769/AS%3A1030579663433730%401622720747470/download/Document1.pdf
-function rotMatrix(C,r)
+function rotMatrix(C,r; checkEigen = false, tol = 1e-3)
     T = [
-        r[1,1]^2 r[2,1]^2 r[3,1]^3 2*r[1,1]*r[2,1] 2*r[2,1]*r[3,1] 2*r[3,1]*r[1,1];
-        r[1,2]^2 r[2,2]^2 r[3,2]^2 2*r[1,2]*r[2,2] 2*r[2,2]*r[3,2] 2*r[3,2]*r[1,2];
-        r[1,3]^2 r[2,3]^2 r[3,3]^2 2*r[1,3]*r[2,3] 2*r[2,3]*r[3,3] 2*r[3,3]*r[1,3];
-        r[1,1]*r[1,2] r[2,1]*r[2,2] r[3,1]*r[3,2] r[1,1]*r[2,2]+r[1,2]*r[2,1] r[2,1]*r[3,2] + r[3,1]*r[2,2] r[3,1]*r[1,2]+r[3,2]*r[1,1];
-        r[1,2]*r[1,3] r[2,2]*r[2,3] r[3,3]*r[3,2] r[2,3]*r[1,2]+r[1,3]*r[2,2] r[2,2]*r[3,3] + r[3,2]*r[2,3] r[3,2]*r[1,3]+r[1,2]*r[3,3];
-        r[1,1]*r[1,3] r[2,3]*r[2,1] r[3,3]*r[3,1] r[1,3]*r[2,1]+r[1,1]*r[2,3] r[2,3]*r[3,1] + r[2,1]*r[3,3] r[3,3]*r[1,1]+r[3,1]*r[1,3]]
+        r[1,1]^2 r[1,2]^2 r[1,3]^2 2*r[1,1]*r[1,2] 2*r[1,2]*r[1,3] 2*r[1,3]*r[1,1];
+        r[2,1]^2 r[2,2]^2 r[2,3]^2 2*r[2,1]*r[2,2] 2*r[2,2]*r[2,3] 2*r[2,3]*r[2,1];
+        r[3,1]^2 r[3,2]^2 r[3,3]^2 2*r[3,1]*r[3,2] 2*r[3,2]*r[3,3] 2*r[3,3]*r[3,1];
+        r[1,1]*r[2,1] r[1,2]*r[2,2] r[1,3]*r[2,3] r[1,1]*r[2,2]+r[2,1]*r[1,2] r[1,2]*r[2,3] + r[1,3]*r[2,2] r[1,3]*r[2,1]+r[2,3]*r[1,1];
+        r[2,1]*r[3,1] r[2,2]*r[3,2] r[3,3]*r[2,3] r[3,2]*r[2,1]+r[3,1]*r[2,2] r[2,2]*r[3,3] + r[2,3]*r[3,2] r[2,3]*r[3,1]+r[2,1]*r[3,3];
+        r[1,1]*r[3,1] r[3,2]*r[1,2] r[3,3]*r[1,3] r[3,1]*r[1,2]+r[1,1]*r[3,2] r[3,2]*r[1,3] + r[1,2]*r[3,3] r[3,3]*r[1,1]+r[1,3]*r[3,1]]
 
-    return inv(T)*C*T
+
+
+    T[4:6,1:3] .= T[4:6,1:3].*sqrt(2)
+    T[1:3,4:6] .= T[1:3,4:6]./sqrt(2)
+
+    cPrime = inv(T)*C*T
+    if checkEigen
+        eigs = (eigvals(C),eigvals(cPrime))
+        I1 = [eigs[i][1]+eigs[i][2]+eigs[i][3] for i in 1:2]
+        I2 = [eigs[i][1]*eigs[i][2]+eigs[i][1]*eigs[i][3]+eigs[i][3]*eigs[i][2] for i in 1:2]
+        I3 = [eigs[i][1]*eigs[i][2]*eigs[i][3] for i in 1:2]
+        Is = (I1,I2,I3)
+        for I in Is
+            @assert(abs((I[1]-I[2])/I[1])<tol,"Eigenvalue error in rotation")
+        end
+
+        for i in 1:3
+            for j in i:3
+                @assert(abs((cPrime[i,j]-cPrime[j,i])/cPrime[i,j] < tol),"Symmetry error in rotation")
+            end
+        end
+    end
+    
+    return cPrime
+    #return fullToVoigt(rotTensor(voigtToFull(C),r))
 end
 function rotTensor(C,g)
     Tprime = zeros(3,3,3,3)
-    #=
+    
     for k = 1:3
         for l = 1:3
             for s = 1:3
                 for t = 1:3
 
-                    rotatedTensor[k,l,s,t] = 0
+                    Tprime[k,l,s,t] = 0
 
                     for m = 1:3
                         for n = 1:3
                             for p = 1:3
                                 for r = 1:3
 
-                                    rotatedTensor[k,l,s,t] += c[m,n,p,r] * T[k, m] * T[l, n] * T[s, p] * T[t, r]
+                                    Tprime[k,l,s,t] += c[m,n,p,r] * T[k, m] * T[l, n] * T[s, p] * T[t, r]
 
                                 end
                             end
@@ -158,7 +182,8 @@ function rotTensor(C,g)
                 end
             end
         end
-    end=#
+    end
+    #=
     for i in 1:3
         for j in 1:3
             for k in 1:3
@@ -175,7 +200,7 @@ function rotTensor(C,g)
                 end
             end
         end
-    end
+    end=#
     return Tprime
 end
 
